9b47f26ae3178b1126bd8f254b731278e69b8d91
Allison Nolan <allisonknolan@gmail.com>
2020-11-11 10:24:36 -0600

    commit tree 176bfd09dee43b766e1cacd086885ba71c87ff8d

---
+++ greetings.rb/greetings.rb
@@ -0,0 +1,13 @@
+def say_hello_world_ten_times
+    greeting = "Hello World"
+    puts greeting
+    puts greeting
+    puts greeting
+    puts greeting
+    puts greeting
+    puts greeting
+    puts greeting
+    puts greeting
+    puts greeting
+    puts greeting
+end
